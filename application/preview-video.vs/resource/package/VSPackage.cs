161a163be8af41bc0666e9afdc806ea74e1f67b9
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2021-08-31 19:24:25 +0300

    - Some libraries are updated to newer versions

--- application/preview-video.vs/resource/package/VSPackage.cs
+++ application/preview-video.vs/resource/package/VSPackage.cs
@@ -19,7 +19,7 @@ namespace resource.package
             public const string DESCRIPTION = "Quick preview the most popular video files";
             public const string GUID = "DF3B9597-5265-4147-9042-13AC92159207";
             public const string NAME = "Preview-VIDEO";
-            public const string VERSION = "1.0.2";
+            public const string VERSION = "1.0.4";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
