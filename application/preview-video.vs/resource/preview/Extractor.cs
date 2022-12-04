7c36034cabac8cea49f978ff1a6c9141f6158017
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2022-12-04 22:38:27 +0200

    - Some libraries are updated to newer versions - README file is updated

--- application/preview-video.vs/resource/preview/Extractor.cs
+++ application/preview-video.vs/resource/preview/Extractor.cs
@@ -10,13 +10,13 @@ namespace resource.preview
         {
             var a_Context = ImageMetadataReader.ReadMetadata(file);
             {
-                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.HEADER, level, "[[[Info]]]");
+                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.HEADER, level, "[[[Info]]]");
                 {
-                    context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 1, "[[[File Name]]]", url);
-                    context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 1, "[[[File Size]]]", (new FileInfo(file)).Length.ToString());
-                    context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 1, "[[[Media Types]]]", "Audio, Video");
-                    context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 1, "[[[Raw Format]]]", __GetString(a_Context, "Expected File Name Extension").ToUpper());
-                    context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 1, "[[[Tag Types]]]", __GetString(a_Context, "Detected File Type Name"));
+                    context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 1, "[[[File Name]]]", url);
+                    context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 1, "[[[File Size]]]", (new FileInfo(file)).Length.ToString());
+                    context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 1, "[[[Media Types]]]", "Audio, Video");
+                    context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 1, "[[[Raw Format]]]", __GetString(a_Context, "Expected File Name Extension").ToUpper());
+                    context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 1, "[[[Tag Types]]]", __GetString(a_Context, "Detected File Type Name"));
                 }
             }
             {
@@ -28,44 +28,44 @@ namespace resource.preview
                 for (var i = 0; i < a_Size; i++)
                 {
                     context.
-                        Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PREVIEW, level);
+                        Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PREVIEW, level);
                 }
             }
             {
-                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.FOOTER, level, "[[[Size]]]: " + __GetInteger(a_Context, "Width").ToString() + " x " + __GetInteger(a_Context, "Height").ToString());
+                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.FOOTER, level, "[[[Size]]]: " + __GetInteger(a_Context, "Width").ToString() + " x " + __GetInteger(a_Context, "Height").ToString());
                 {
-                    context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.FOLDER, level + 1, "[[[Codecs]]]");
+                    context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.FOLDER, level + 1, "[[[Codecs]]]");
                     {
                         context.
                             SetComment("[[[Audio]]]", "[[[Media Type]]]").
-                            Send(NAME.SOURCE.PREVIEW, NAME.TYPE.FILE, level + 2, __GetString(a_Context, "Expected File Name Extension").ToUpper() + " Audio (" + __GetString(a_Context, "Major Brand") + ")");
+                            Send(NAME.SOURCE.PREVIEW, NAME.EVENT.FILE, level + 2, __GetString(a_Context, "Expected File Name Extension").ToUpper() + " Audio (" + __GetString(a_Context, "Major Brand") + ")");
                         {
-                            context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 3, "[[[Header]]]");
+                            context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 3, "[[[Header]]]");
                             {
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Channels]]]", __GetChannels(a_Context).ToString());
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Duration]]]", __GetDuration(a_Context));
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Media Types]]]", "[[[Audio]]]");
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Preferred Rate]]]", __GetInteger(a_Context, "Preferred Rate").ToString());
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Preferred Volume]]]", __GetInteger(a_Context, "Preferred Volume").ToString());
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Channels]]]", __GetChannels(a_Context).ToString());
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Duration]]]", __GetDuration(a_Context));
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Media Types]]]", "[[[Audio]]]");
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Preferred Rate]]]", __GetInteger(a_Context, "Preferred Rate").ToString());
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Preferred Volume]]]", __GetInteger(a_Context, "Preferred Volume").ToString());
                             }
                         }
                     }
                     {
                         context.
                             SetComment("[[[Video]]]", "[[[Media Type]]]").
-                            Send(NAME.SOURCE.PREVIEW, NAME.TYPE.FILE, level + 2, __GetString(a_Context, "Expected File Name Extension").ToUpper() + " Video (" + __GetString(a_Context, "Major Brand") + ")");
+                            Send(NAME.SOURCE.PREVIEW, NAME.EVENT.FILE, level + 2, __GetString(a_Context, "Expected File Name Extension").ToUpper() + " Video (" + __GetString(a_Context, "Major Brand") + ")");
                         {
-                            context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 3, "[[[Header]]]");
+                            context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 3, "[[[Header]]]");
                             {
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Duration]]]", __GetDuration(a_Context));
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Media Types]]]", "[[[Video]]]");
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Duration]]]", __GetDuration(a_Context));
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Media Types]]]", "[[[Video]]]");
                             }
                         }
                         {
-                            context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 3, "[[[Size]]]");
+                            context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 3, "[[[Size]]]");
                             {
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Width]]]", __GetInteger(a_Context, "Width").ToString());
-                                context.Send(NAME.SOURCE.PREVIEW, NAME.TYPE.PARAMETER, level + 4, "[[[Height]]]", __GetInteger(a_Context, "Height").ToString());
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Width]]]", __GetInteger(a_Context, "Width").ToString());
+                                context.Send(NAME.SOURCE.PREVIEW, NAME.EVENT.PARAMETER, level + 4, "[[[Height]]]", __GetInteger(a_Context, "Height").ToString());
                             }
                         }
                     }
