5a0fd51104d0675e9669ffd8f462a65c79b85476
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-04-27 17:56:01 +0300

    - Minor changes

--- application/preview-video.vs/resource/package/VSPackage.cs
+++ application/preview-video.vs/resource/package/VSPackage.cs
@@ -20,6 +20,7 @@ namespace resource.package
         internal static class CONSTANT
         {
             public const string APPLICATION = "Visual Studio";
+            public const string COMPANY = "Viacheslav Lozinskyi";
             public const string COPYRIGHT = "Copyright (c) 2020-2023 by Viacheslav Lozinskyi. All rights reserved.";
             public const string DESCRIPTION = "Quick preview the most popular video files";
             public const string GUID = "DF3B9597-5265-4147-9042-13AC92159207";
