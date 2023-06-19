8b3636c5ed50fa549b8548aa96d80b6ff1f4d541
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-06-19 07:28:45 +0300

    - Adaptation to new version of MetaOutput.Client (3.0.0)

--- application/preview-video.vs/resource/package/VSPackage.cs
+++ application/preview-video.vs/resource/package/VSPackage.cs
@@ -1,11 +1,6 @@
-﻿
-using EnvDTE;
-using EnvDTE80;
-using Microsoft.VisualStudio;
+﻿using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
@@ -31,67 +26,29 @@ namespace resource.package
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            {
-                extension.AnyPreview.Connect();
-                extension.AnyPreview.Register(".3G2", new resource.preview.Extractor());
-                extension.AnyPreview.Register(".3GP", new resource.preview.Extractor());
-                extension.AnyPreview.Register(".ASF", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".AVI", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".F4V", new resource.preview.Extractor());
-                extension.AnyPreview.Register(".M4V", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".MKV", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".MOV", new resource.preview.Extractor());
-                extension.AnyPreview.Register(".MP4", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".MPEG", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".MPG", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".MP1", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".MP2", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".OGV", new resource.preview.Taglib());
-                extension.AnyPreview.Register(".WMV", new resource.preview.Taglib());
-            }
-            {
-                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            }
-            try
-            {
-                if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
-                {
-                    var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
-                    if (a_Context != null)
-                    {
-                        var a_Context1 = (OutputWindowPane)null;
-                        for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
-                        {
-                            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == CONSTANT.HOST)
-                            {
-                                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
-                                break;
-                            }
-                        }
-                        if (a_Context1 == null)
-                        {
-                            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add(CONSTANT.HOST);
-                        }
-                        if (a_Context1 != null)
-                        {
-                            a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it --> https://www.metaoutput.net/download\r\n");
-                            a_Context1.Activate();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.ToString());
-            }
+            extension.AnyPreview.Connect(CONSTANT.APPLICATION, CONSTANT.NAME);
+            extension.AnyPreview.Register(".3G2", new resource.preview.Extractor());
+            extension.AnyPreview.Register(".3GP", new resource.preview.Extractor());
+            extension.AnyPreview.Register(".ASF", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".AVI", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".F4V", new resource.preview.Extractor());
+            extension.AnyPreview.Register(".M4V", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".MKV", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".MOV", new resource.preview.Extractor());
+            extension.AnyPreview.Register(".MP4", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".MPEG", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".MPG", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".MP1", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".MP2", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".OGV", new resource.preview.Taglib());
+            extension.AnyPreview.Register(".WMV", new resource.preview.Taglib());
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
         }
 
         protected override int QueryClose(out bool canClose)
         {
-            {
-                extension.AnyPreview.Disconnect();
-                canClose = true;
-            }
+            extension.AnyPreview.Disconnect();
+            canClose = true;
             return VSConstants.S_OK;
         }
     }
